085715fe2514b63a6500a5c78100470edfa4c864
Wesley <coetzeewr@gmail.com>
2026-01-26 09:03:38 +1300

    chore: add lint precommit

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ["node_modules/**", "dist/**", "prisma/**"],
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/no-explicit-any": "error",
+    },
+  }
+);
